7f5b81cfa80f51cd6b82984abf3964f048249f17
Kevin Sencherey <k.sencherey@node4.co.uk>
2025-01-14 16:43:58 +0000

    Util function added to format money

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,5 +1,6 @@
 import { cart } from '../data/cart.js'
 import { products } from '../data/products.js'
+import { formatCurrency } from './utils/money.js'
 
 let carSummaryHTML = ''
 
@@ -25,7 +26,7 @@ cart.forEach((cartItem) => {
                     ${matchingProduct.name}
                 </div>
                 <div class="product-price">
-                    £${matchingProduct.priceCents / 100}
+                    £${formatCurrency(matchingProduct.priceCents)}
                 </div>
                 <div class="product-quantity">
                     <span>
