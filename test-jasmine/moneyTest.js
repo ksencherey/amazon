e4901a0a686166fd1438cc65b12513e6a70e5fb6
Kevin Sencherey <kevinsencherey@Kevins-MacBook-Pro.local>
2025-02-17 11:18:40 +0000

    Remove default tests and add money tests

---
+++ test-jasmine/moneyTest.js
@@ -0,0 +1,19 @@
+import formatCurrency from "../scripts/utils/money.js";
+
+
+// Create a test suite
+describe('test suite: formatCurrency', () => {
+    //create test
+    it('converts pence into pounds', () => {
+        expect(formatCurrency(2095)).toEqual('20.95')
+    })
+
+
+    it('works with 0', () => {
+        expect(formatCurrency(0)).toEqual('0.00')
+    })
+
+    it('rounds up to the nearest pence', () => {
+        expect(formatCurrency(2000.5)).toEqual('20.01')
+    })
+})
--- tests/moneyTest.js
+++ tests/moneyTest.js
@@ -9,7 +9,7 @@ if (formatCurrency(2095) === '20.95'){
 }
 
 console.log('Works with 0')
-if (formatCurrency(0) === '00.00'){
+if (formatCurrency(0) === '0.00'){
     console.log('passed')
 } else {
     console.log('failed')
