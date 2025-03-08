cd589e0be79468326b00f01a2ed91cfb4896fbb8
Kevin Sencherey <kevinsencherey@Kevins-MacBook-Pro.local>
2025-03-08 05:19:50 +0000

    Convert products to class

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency} from "../scripts/utils/money.js";
+
 export function getProduct(productId){
   let matchingProduct;
   products.forEach((product) => {
@@ -6,6 +8,31 @@ export function getProduct(productId){
       }
   })
   return matchingProduct
+}
+
+class Product {
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
+
+  constructor(productDetails)
+{
+  this.id = productDetails.id
+  this.image = productDetails.image
+  this.name = productDetails.name
+  this.rating = productDetails.rating
+  this.priceCents = productDetails.priceCents
+}
+
+  getStarsUrl(){
+     return `images/ratings/rating-${this.rating.stars * 10}.png`
+  }
+
+  getPrice(){
+    return `£${formatCurrency(this.priceCents)}`
+  }
 }
 
 
@@ -668,4 +695,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+  return new Product(productDetails)
+});
