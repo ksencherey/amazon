da2dd7686e9ac24ca92af585094ad40678eeb4e3
Kevin Sencherey <kevinsencherey@Kevins-MacBook-Pro.local>
2025-03-17 14:26:32 +0000

    Learn more details about classes and this

--- data/products.js
+++ data/products.js
@@ -49,12 +49,38 @@ class Clothing extends Product {
   }
 
   extraInfoHTML() {
-    super.extraInfoHTML()
+    // super.extraInfoHTML()
     return `
       <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
     `
   }
 }
+
+/*
+const date = new Date()
+console.log(date)
+console.log(date.toLocaleTimeString())
+console.log(date.toLocaleDateString())
+
+console.log(this)
+const object2 = {
+  a: 2,
+  b: this.a
+}
+
+function logThis(){
+  console.log(this)
+}
+logThis()
+logThis.call('hello')
+
+const object3 = {
+  method: () =>{
+    console.log(this)
+  }
+}
+object3.method()
+*/
 
 
 export const products = [
