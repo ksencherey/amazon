b2819e97a1bcbb0afb36d49d14ab18aab45b660b
Kevin Sencherey <kevinsencherey@Kevins-MacBook-Pro.local>
2025-03-17 16:26:11 +0000

    Load producst from the backend

--- data/products.js
+++ data/products.js
@@ -82,7 +82,31 @@ const object3 = {
 object3.method()
 */
 
+// Load products from backend
+export let products = []
 
+export function loadProducts(fun){
+  const xhr = new XMLHttpRequest()
+
+  xhr.addEventListener('load', () => {
+    products = JSON.parse(xhr.response).map((productDetails) => {
+      if(productDetails.type === 'clothing'){
+        return new Clothing(productDetails)
+      }
+      return new Product(productDetails)
+    });
+    console.log('load products')
+
+    fun()
+  })
+
+  xhr.open('GET', 'https://supersimplebackend.dev/products')
+  xhr.send()
+}
+
+
+
+/*
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -748,3 +772,4 @@ export const products = [
   }
   return new Product(productDetails)
 });
+*/
